ee2c536fa88c12255a018a1436a64fcbd30f9035
Luiz Gabriel Lisboa Santos <lzg.lisboasantos5569@gmail.com>
2024-10-04 12:59:36 -0300

    migrando os modelos básicos do django e do pacote allauth, registrando o app no github e configurando o OAuth para permitir a autenticação

--- setup/settings.py
+++ setup/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,6 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -59,15 +63,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddle',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 # Provider specific settings
 SOCIALACCOUNT_PROVIDERS = {
     'github': {
         'APP': {
-            'client_id': '123',
-            'secret': '456',
+            'client_id': env('GITHUB_CLIENT_ID'),
+            'secret': env('GITHUB_CLIENT_SECRET'),
             'key': ''
         }
     }
